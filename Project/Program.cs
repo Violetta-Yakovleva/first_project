ce0d606516b3fe6bc678359ce5423247c518022d
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-04-28 16:16:12 +0500

    Решение заданной задачи

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// // лучше обойтись исключительно массивами.
+
+string[] CreateStringArray(int size)
+{
+    string[] array = new string[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i+1}-ое значение массива: ");
+        string text = Convert.ToString(Console.ReadLine());
+        array[i] = text;
+    }
+    return array;
+}
+
+string[] CreateNewArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+        int index = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Укажите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] myArray = CreateStringArray(size);
+CreateNewArray(myArray);
+ShowArray(CreateNewArray(myArray));
